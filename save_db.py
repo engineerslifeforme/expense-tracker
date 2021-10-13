bc01f85a726acdefe67b5a6a4a01943ae970cdad
Creative Rigor, LLC <creativerigor@gmail.com>
2021-10-12 22:34:49 -0400

    Changes to make saving DB clearer

--- save_db.py
+++ save_db.py
@@ -14,10 +14,14 @@ if action == 'upload':
     print('Uploading...')
     date_str = date.today().strftime("%Y%m%d")
     new_file = SERVER / f'{date_str}.db'
+    print(f'Saving file to {new_file}')
     shutil.copyfile(LOCAL_FILE, new_file)
 elif action == 'download':
     print('Downloading...')
-    server_file = next(SERVER.glob('*.db'))
+    server_file_list = list(SERVER.glob('*.db'))
+    server_file_list.sort()
+    server_file = server_file_list[-1]
+    print(f'Copying {server_file}')
     shutil.copyfile(server_file, LOCAL_FILE)
 else:
     print(f'Unknown command {action}')
