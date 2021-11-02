e5ab16bf18b66913416903fb0a190b010239fcdb
Creative Rigor, LLC <creativerigor@gmail.com>
2021-11-01 20:59:52 -0400

    Adding artifacts to save

--- save_db.py
+++ save_db.py
@@ -8,7 +8,14 @@ import sys
 action = sys.argv[1]
 
 SERVER = Path('/Volumes/Media/Expense')
+ASSETS = SERVER / 'assets'
 LOCAL_FILE = 'example.db'
+
+ASSET_LIST = [
+    'statement_scaler.joblib',
+    'statement_to_category_model.joblib',
+    'word_list.joblib',
+]
 
 if action == 'upload':
     print('Uploading...')
@@ -16,6 +23,11 @@ if action == 'upload':
     new_file = SERVER / f'{date_str}.db'
     print(f'Saving file to {new_file}')
     shutil.copyfile(LOCAL_FILE, new_file)
+    for asset_file in ASSET_LIST:
+        shutil.copyfile(
+            asset_file,
+            ASSETS / asset_file
+        )
 elif action == 'download':
     print('Downloading...')
     server_file_list = list(SERVER.glob('*.db'))
@@ -23,5 +35,10 @@ elif action == 'download':
     server_file = server_file_list[-1]
     print(f'Copying {server_file}')
     shutil.copyfile(server_file, LOCAL_FILE)
+    for asset_file in ASSET_LIST:
+        shutil.copyfile(
+            ASSETS / asset_file,
+            asset_file
+        )
 else:
     print(f'Unknown command {action}')
