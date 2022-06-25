64c2eb2e0452da1626dbb5d97e6ed89300dc3502
Creative Rigor, LLC <creativerigor@gmail.com>
2022-06-25 15:31:21 -0400

    small changes

--- newdb_access.py
+++ newdb_access.py
@@ -690,18 +690,23 @@ class DbAccess(object):
         self.update_budget_by_budget(increment, budget_id)
         return new_id
 
+    def get_monthly_budget_increment(budget_info: dict) -> Decimal:
+        increment = budget_info['increment']
+        if budget_info['frequency'] == 'Y':
+            increment = (increment / Decimal('12.00')).quantize(Decimal('1.00'))
+        elif budget_info['frequency'] == 'D':
+            increment = ((increment * Decimal('365.0')) / Decimal('12.0')).quantize(Decimal('1.00'))
+        elif budget_info['frequency'] == 'W':
+            increment = ((increment * Decimal('52.0')) / Decimal('12.0')).quantize(Decimal('1.00'))
+        return increment
+
     def update_budgets(self):
         budget_dicts = self.get_budgets().to_dict(orient='records')
         for budget_info in budget_dicts:
             increment = budget_info['increment']
             if increment != ZERO and budget_info['valid'] == 1:
                 budget_id = budget_info['id']
-                if budget_info['frequency'] == 'Y':
-                    increment = (increment / Decimal('12.00')).quantize(Decimal('1.00'))
-                elif budget_info['frequency'] == 'D':
-                    increment = ((increment * Decimal('365.0')) / Decimal('12.0')).quantize(Decimal('1.00'))
-                elif budget_info['frequency'] == 'W':
-                    increment = ((increment * Decimal('52.0')) / Decimal('12.0')).quantize(Decimal('1.00'))
+                increment = get_monthly_budget_increment(budget_info)
                 self.adjust_budget(increment, budget_id)
 
     def set_budget_increment(self, increment: Decimal, budget_id: int):
